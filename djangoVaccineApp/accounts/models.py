7a20c52e01f85d8b77dffe80ef170ca6d3bdf874
jojo <yumengch2020@gmail.com>
2021-04-25 17:27:12 -0400

    add city attribute

--- djangoVaccineApp/accounts/models.py
+++ djangoVaccineApp/accounts/models.py
@@ -14,6 +14,7 @@ class Patient(models.Model):
     phone_number = models.CharField(max_length=20)
     addressLine1 = models.CharField(max_length=20)
     ssn = models.CharField(max_length=20)
+    city = models.CharField(max_length=20)
 
 
 class Provider(models.Model):
