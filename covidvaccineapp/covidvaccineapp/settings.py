6bb9a7611e1d0ddde58d7a17ca769de270e00684
Chuanqi Xiong <40122680+Edward0o0@users.noreply.github.com>
2021-04-25 16:22:54 -0400

    changed django database to sqlite3

--- covidvaccineapp/covidvaccineapp/settings.py
+++ covidvaccineapp/covidvaccineapp/settings.py
@@ -82,15 +82,8 @@ WSGI_APPLICATION = "covidvaccineapp.wsgi.application"
 
 DATABASES = {
     "default": {
-        "ENGINE": "django.db.backends.mysql",
-        "NAME": "covidvaccineapp",
-        "OPTIONS": {
-            "init_command": "SET sql_mode='STRICT_TRANS_TABLES'",
-        },
-        "USER": "root",
-        "PASSWORD": "",
-        "HOST": "localhost",
-        "PORT": "3306",
+        "ENGINE": "django.db.backends.sqlite3",
+        "NAME": BASE_DIR / "db.sqlite3",
     }
 }
 
