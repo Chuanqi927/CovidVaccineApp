4b6bb87007ac7d10866114674bab81d8edfcb1b4
Chuanqi Xiong <40122680+Edward0o0@users.noreply.github.com>
2021-04-25 16:57:06 -0400

    applied black

--- covidvaccineapp/user/views.py
+++ covidvaccineapp/user/views.py
@@ -13,52 +13,54 @@ from .models import User
 
 
 def register(request):
-    return render(request, '../templates/register.html')
+    return render(request, "../templates/register.html")
 
 
 class patient_register(CreateView):
     model = User
     form_class = PatientSignUpForm
-    template_name = '../templates/patient_register.html'
+    template_name = "../templates/patient_register.html"
 
     def form_valid(self, form):
         user = form.save()
         login(self.request, user)
-        return redirect('/')
+        return redirect("/")
 
 
 class provider_register(CreateView):
     model = User
     form_class = ProviderSignUpForm
-    template_name = '../templates/provider_register.html'
+    template_name = "../templates/provider_register.html"
 
     def form_valid(self, form):
         user = form.save()
         login(self.request, user)
-        return redirect('/')
+        return redirect("/")
 
 
 def login_request(request):
-    if request.method == 'POST':
+    if request.method == "POST":
         form = AuthenticationForm(data=request.POST)
         if form.is_valid():
-            username = form.cleaned_data.get('username')
-            password = form.cleaned_data.get('password')
+            username = form.cleaned_data.get("username")
+            password = form.cleaned_data.get("password")
             user = authenticate(username=username, password=password)
             if user is not None:
                 login(request, user)
-                return redirect('/')
+                return redirect("/")
             else:
                 messages.error(request, "Invalid username or password")
         else:
             messages.error(request, "Invalid username or password")
-    return render(request, '../templates/login.html',
-                  context={'form': AuthenticationForm()})
+    return render(
+        request, "../templates/login.html", context={"form": AuthenticationForm()}
+    )
 
 
 def logout_view(request):
     logout(request)
-    return redirect('/')
+    return redirect("/")
+
 
 # def user_login(request):
 #     if request.user.is_authenticated:
