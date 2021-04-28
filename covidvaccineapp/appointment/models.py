e88d6ac63b7c345659ed48086ad9a8d4b782c267
Chuanqi Xiong <40122680+Edward0o0@users.noreply.github.com>
2021-04-28 02:25:43 -0400

    finished uploading appointment, still have redirect problem

--- covidvaccineapp/appointment/models.py
+++ covidvaccineapp/appointment/models.py
@@ -24,5 +24,5 @@ class Appointment(models.Model):
 class OfferAppointment(models.Model):
     appointment_id = models.ForeignKey(Appointment, on_delete=models.CASCADE)
     patient = models.ForeignKey("user.Patient", on_delete=models.CASCADE)
-    status = models.CharField(max_length=255)
-    expire_time = models.DateTimeField()
+    status = models.CharField(max_length=255, null=True, blank=True, default=None)
+    expire_time = models.DateTimeField(null=True, blank=True, default=None)
--- covidvaccineapp/appointment/views.py
+++ covidvaccineapp/appointment/views.py
@@ -1,6 +1,8 @@
 from django.http import HttpResponse
-from django.shortcuts import render,redirect
-
+from django.shortcuts import render, redirect
+from user.models import User, Patient, Provider
+from .forms import AppointmentCreationForm
+from django.contrib.auth import authenticate, login as auth_login
 
 # Create your views here.
 def index(request):
@@ -8,10 +10,26 @@ def index(request):
 
 
 def upload(request):
-    if request.method == "POST":
-        print(request.POST)
-        # return redirect("../success")
-    return render(request, "upload_appointments.html")
+    if request.user.is_authenticated:
+        username = request.user.username
+        # print("username :", username)
+        user_id = request.user.id
+        # print("user_id :", user_id)
+        if request.method == "POST" and Provider.objects.filter(user_id=user_id).exists():
+            # print("request.POST :", request.POST)
+            if "provider_id" in request.POST and "appointment_time" in request.POST and "available_number" in request.POST and "slot_id" in request.POST:
+                upload_appointment_form = AppointmentCreationForm(data=request.POST)
+                # print("form created")
+                if upload_appointment_form.is_valid():
+                    # print(upload_appointment_form)
+                    upload_appointment_form.calculate_slot_id()
+                    upload_appointment_form.save()
+                    return redirect("../appointment/success")
+        else:
+            return render(request, "upload_appointments.html")
+    else:
+        return HttpResponse("you are not logged in as provider")
+
 
 
 def success(request):
