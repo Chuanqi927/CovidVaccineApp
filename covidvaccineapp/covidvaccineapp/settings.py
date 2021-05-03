99c1d5e105bd15aa62faa0bdab7530fbc2728e4a
jojo <yumengch2020@gmail.com>
2021-05-03 18:39:25 -0400

    navbar version1

--- covidvaccineapp/covidvaccineapp/settings.py
+++ covidvaccineapp/covidvaccineapp/settings.py
@@ -127,6 +127,10 @@ USE_TZ = True
 
 STATIC_URL = "/static/"
 STATIC_ROOT = os.path.join(BASE_DIR, "static")
+STATICFILES_DIRS = [
+    BASE_DIR / "static"
+]
+STATIC_ROOT = "static_root"
 
 SITE_ID = 1
 
--- covidvaccineapp/covidvaccineapp/views.py
+++ covidvaccineapp/covidvaccineapp/views.py
@@ -2,5 +2,21 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 
-def index(request):
+def homepage(request):
     return render(request, "homepage.html")
+
+
+def index(request):
+    return render(request, "index.html")
+
+
+def index2(request):
+    return render(request, "pulseadmin/index.html")
+
+
+def base(request):
+    return render(request, "base.html")
+
+
+def home(request):
+    return render(request, "home.html")
