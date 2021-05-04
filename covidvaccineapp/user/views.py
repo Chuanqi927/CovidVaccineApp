08ba5f364681b68d4686ab48f33c4521b6750d40
jojo <yumengch2020@gmail.com>
2021-05-04 01:44:24 -0400

    patient edit information

--- covidvaccineapp/user/views.py
+++ covidvaccineapp/user/views.py
@@ -12,6 +12,7 @@ from .forms import PatientSignUpForm, ProviderSignUpForm, UserUpdateForm, \
 from django.contrib.auth.forms import AuthenticationForm
 from .models import User, Patient, Provider
 from staticInfo.models import WeeklyTimeSlot
+from appointment.models import OfferAppointment
 
 
 def sign_up(request):
@@ -80,59 +81,92 @@ def patient_edit_profile(request):
         u_form = UserUpdateForm(instance=request.user)
         p_form = PatientUpdateForm(instance=request.user.patient)
 
-        first_name = request.user.first_name
-        last_name = request.user.last_name
-        username = request.user.username
-        context = {
-            "u_form": u_form,
-            "p_form": p_form,
-            "first_name": first_name,
-            "last_name": last_name,
-            "username": username,
-        }
+    first_name = request.user.first_name
+    last_name = request.user.last_name
+    username = request.user.username
 
-        return render(request, "patient_edit_profile.html", context)
+    u_form = UserUpdateForm(instance=request.user)
+    p_form = PatientUpdateForm(instance=request.user)
+    context2 = {
+        "first_name": first_name,
+        "last_name": last_name,
+        "username": username,
+        "u_form": u_form,
+        "p_form": p_form,
+    }
+
+    return render(request, "patient_edit_profile.html", context2)
 
 
 def patient_profile(request):
+    global context
     if not request.user.is_authenticated:
         return redirect("login")
+    if request.method == 'POST':
+        u_form = UserUpdateForm(request.POST, instance=request.user)
+        p_form = PatientUpdateForm(request.POST, instance=request.user.patient)
+        # pp_form = PatientUpdatePreferenceForm(request.POST, instance=request.user.patient)
+        if u_form.is_valid() and p_form.is_valid():
+            u_form.save()
+            p_form.save()
+
+            messages.success(request, f"Your information has been updated!")
+            return redirect("patient_profile")
+    else:
+        u_form = UserUpdateForm(instance=request.user)
+        p_form = PatientUpdateForm(instance=request.user.patient)
+        # pp_form = PatientUpdatePreferenceForm(instance=request.user.patient)
+
+        if request.method == 'POST':
+            pp_form = PatientUpdatePreferenceForm(request.POST, instance=request.user.patient)
+            if pp_form.is_valid():
+                pp_form.save()
+                messages.success(request, f"Your preference has been updated!")
+                return redirect("patient_profile")
+        else:
+            pp_form = PatientUpdatePreferenceForm(instance=request.user.patient)
+
+    offer_list = list(OfferAppointment.objects.filter(patient_id=request.user.id))
 
     first_name = request.user.first_name
     last_name = request.user.last_name
     username = request.user.username
+    u_form = UserUpdateForm(instance=request.user)
+    p_form = PatientUpdateForm(instance=request.user)
+    pp_form = PatientUpdatePreferenceForm(instance=request.user)
     context = {
+        "u_form": u_form,
+        "p_form": p_form,
+        "pp_form": pp_form,
         "first_name": first_name,
         "last_name": last_name,
         "username": username,
-
+        "offer_list": offer_list,
     }
+
     return render(request, "patient_profile.html", context)
 
 
 def patient_edit_preference(request):
-    if request.method == 'POST':
-        pp_form = PatientUpdatePreferenceForm(request.POST, instance=request.user.patient)
-        if pp_form.is_valid():
-            pp_form.save()
-            messages.success(request, f"Your preference has been updated!")
-            return redirect("patient_profile")
-    else:
-        pp_form = PatientUpdatePreferenceForm(instance=request.user.patient)
+    # if request.method == 'POST':
+    #     pp_form = PatientUpdatePreferenceForm(request.POST, instance=request.user.patient)
+    #     if pp_form.is_valid():
+    #         pp_form.save()
+    #         messages.success(request, f"Your preference has been updated!")
+    #         return redirect("patient_profile")
+    # else:
+    #     pp_form = PatientUpdatePreferenceForm(instance=request.user.patient)
 
     first_name = request.user.first_name
     last_name = request.user.last_name
     username = request.user.username
-
-    context = {
+    contextpp = {
         "first_name": first_name,
         "last_name": last_name,
         "username": username,
-        "pp_form": pp_form,
-
 
     }
-    return render(request, "patient_edit_preference.html", context)
+    return render(request, "patient_edit_preference.html", contextpp)
 
 
 def provider_profile(request):
